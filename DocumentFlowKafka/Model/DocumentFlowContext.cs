c0817213208fe105fa0f2a6298f695030013308e
Alexander <Descriminant@gmail.com>
2026-09-16 20:43:15 +0500

    Завёл контейнер с kafka , zookepeer и  postgres. postgres базу завёл был конфликто с локльной службой выключил всё завелось корректно. kafka/zookepeer не посмотрел, что там;

---
+++ DocumentFlowKafka/Model/DocumentFlowContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DocumentFlowKafka.Model
+{
+    public class DocumentFlowContext : DbContext
+    {
+
+        public DocumentFlowContext(DbContextOptions<DocumentFlowContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ DocumentFlowKafka/Program.cs
@@ -0,0 +1,41 @@
+using Confluent.Kafka;
+using DocumentFlowKafka.Model;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<DocumentFlowContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddSingleton<IProducer<Null, string>>(sp =>
+{
+    var config = new ProducerConfig
+    {
+        BootstrapServers = builder.Configuration["Kafka:BootstrapServers"]
+    };
+    return new ProducerBuilder<Null, string>(config).Build();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
